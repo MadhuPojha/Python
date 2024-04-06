7339151458ea4efb2c5aadc61f755e83c077a890
MadhuPojha <165268887+MadhuPojha@users.noreply.github.com>
2024-04-06 22:07:14 +0200

    Create Day2.py

---
+++ Learning_with_Udemy/Day2.py
@@ -0,0 +1,53 @@
+#Instance, Class, Local Variables
+class House():
+    counter = 0     # class variable
+    def __init__(self, address, area, price):
+        self.address = address  #instance variable
+        self.area = area        #instance variable
+        self.price = price      #instance variable
+        House.counter += 1  #increasing class variable
+
+        House.quality = 'low'       # creating class var
+        self.quality = 'medium'     # class var into instance var
+        quality = 'high'            # local var
+        print(House.quality, self.quality, quality)
+    def present(self):
+        print('The house at', self.address, 'has an area of', self.area, 'and cose', self.price)
+    
+house = House('Jungfru 9', 80, 600000)  #output: The house at Jungfru 9 has an area of 80 and cose 600000
+house.present()
+print('Class Var:', House.counter)  #Class Var
+print(House.quality)        #printing class var
+#print(self.quality, quality) we cannot access these two var, Not allowed outside the class
+#-----------------------------------------------------------------------------------------------------------------------------------
+
+#OOP-Method
+class Doctor():
+    def __init__(self, fName='John', lName='Smith'):
+        self.fName = fName
+        self.lName = lName
+        self.__format_names()
+
+    def __format_names(self):
+        self.fName = self.fName.title()
+        self.lName = self.lName.title()
+
+    def introduce(self):
+        print('Hi, I am', self.fName) #output: Hi, I am Alexander
+
+    def compare_name(self, name_to_compare):
+        if self.fName == self.compare_name:
+            print('We have the same name!')
+        else:
+            print('Sorry, my name is different..')
+
+    def get_first_last_name_together(self):
+        return self.fName + ' ' + self.lName
+doc = Doctor('Alexander', 'Smith')
+doc.introduce()
+doc.compare_name('John')
+print(doc.__dict__)     #output: {'fName': 'Alexander', 'lName': 'Smith'}
+#----------------------------------------------------------------------------------------------------------------------------------
+
+
+
